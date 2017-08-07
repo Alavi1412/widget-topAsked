25561c7cf85101c61040d9ed919285e1c4cc4489
Seyed Mohammad Hassan Alavi <alavi_hassan@yahoo.com>
2017-08-07 11:53:49 +0430

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -8,4 +8,5 @@ after_initialize do
   #SiteSetting.class_eval do
   #  @choices[:layouts_sidebar_right_widgets].push('widget-topasked')
   #end
+  DiscourseLayouts::WidgetHelper.add_widget('widget-topasked')
 end
